63413c869537af52dd04b3d69e1fddf203904f32
uRuiz <zaszur@gmail.com>
2021-05-16 22:34:45 +0200

    Added screen colors

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -9,10 +9,18 @@ interface iScreenProps {
 export const Screen = ({password}: iScreenProps) => {
   const regex = /[\s\S](?!$)/g;
   const encryptedPassword = (SCREEN_MESSAGES.includes(password)) ? password : password.replace(regex, '*');
+  let messageColor;
+  if (password === 'ERROR'){
+    messageColor = 'input__error';
+  } else if (password === 'LOCKED'){
+    messageColor = 'input__locked';
+  } else if (password === 'OK'){
+    messageColor = 'input__ok';
+  }
 
   return (
     <div className='input-container'>
-      <input type='text' disabled value={encryptedPassword}/>
+      <input type='text' disabled value={encryptedPassword} className={messageColor}/>
     </div>
   )
 }
